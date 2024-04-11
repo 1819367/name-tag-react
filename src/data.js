c8c88beb505b6e9d0387d99e10ae1f8c4674dcf4
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-04-10 17:25:13 -0700

    inital commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const nameData = [
+    { id: "1", person: "Elizabeth" },
+    { id: "2", person: "Joy" },
+    { id: "3", person: "Heidi" },
+    { id: "4", person: "Kerry" },
+    { id: "5", person: "Anita" },
+    { id: "6", person: "Jaime" }
+];
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NameTag from "./NameTag";
+import "./styles.css";
+import { nameData } from "./data.js";
+
+const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
+
+const App = () => {
+  const NameTagElements = nameData.map(renderNameTag);
+  return (
+  <div className="App">
+    <h1>Name Tag Generator</h1>
+    {NameTagElements}
+  </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
